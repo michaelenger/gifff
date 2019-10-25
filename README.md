e95cef277867d56d9a478314353e616be8462547
Michael Enger <michaelenger@live.com>
2019-10-25 19:49:50 +0100

    Add config file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Giphy
+
+Find gifs from [Giphy](https://giphy.com/).
+
+## Requirements
+
+* Rust 1.38
+
+## Usage
+
+Copy (and then modify) the config file:
+
+```shell
+cp config.example.toml config.toml
+```
+
+Run it 🤙
+
+```shell
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    api_key: String,
+}
+
+fn read_config(path: &str) -> Result<Config, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let config: Config = toml::from_str(&contents)?;
+
+    Ok(config)
+}
+
 fn main() {
     println!("Hello, world!");
+    match read_config("config.toml") {
+        Err(e) => {
+            println!("Error! Unable to read config.toml file: {}", e);
+        },
+        Ok(config) => { dbg!(config.api_key); },
+    }
 }
