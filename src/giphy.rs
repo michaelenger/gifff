83150e5de867aba450411374ea18a753a0885e2d
Michael Enger <michaelenger@live.com>
2019-11-07 22:20:19 +0000

    Move search function into separate module

---
+++ src/giphy.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+use reqwest::Url;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+/// An image
+pub struct Image {
+    pub frames: Option<String>,
+    pub hash: Option<String>,
+    pub height: Option<String>,
+    pub mp4: Option<String>,
+    pub mp4_size: Option<String>,
+    pub size: Option<String>,
+    pub url: Option<String>,
+    pub webp: Option<String>,
+    pub webp_size: Option<String>,
+    pub width: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+/// A Giphy gif
+pub struct Giphy {
+    pub images: HashMap<String, Image>,
+}
+
+#[derive(Deserialize, Debug)]
+struct GiphySearchResponse {
+    data: Vec<Giphy>,
+}
+
+/// Search Giphy for gifs specified by the query
+pub fn search(api_key: &str, query: &str) -> Result<Vec<Giphy>, reqwest::Error> {
+    let url = Url::parse_with_params(
+        "https://api.giphy.com/v1/gifs/search",
+        &[
+            ("api_key", api_key),
+            ("q", query),
+            ("limit", "50"),
+            ("rating", "g"),
+        ],
+    )
+    .unwrap();
+
+    let body: GiphySearchResponse = reqwest::get(url)?.json()?;
+
+    return Ok(body.data);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,34 @@
-use std::collections::HashMap;
-
-use clap::{Arg, App};
+use clap::{App, Arg};
 use rand::{thread_rng, Rng};
-use reqwest::Url;
-use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-struct Image {
-    frames: Option<String>,
-    hash: Option<String>,
-    height: Option<String>,
-    mp4: Option<String>,
-    mp4_size: Option<String>,
-    size: Option<String>,
-    url: Option<String>,
-    webp: Option<String>,
-    webp_size: Option<String>,
-    width: Option<String>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Giphy {
-    images: HashMap<String, Image>,
-}
-
-#[derive(Deserialize, Debug)]
-struct GiphyResponse {
-    data: Vec<Giphy>,
-}
-
-fn make_request(api_key: &str, query: &str) -> Result<Vec<Giphy>, reqwest::Error> {
-    let url = Url::parse_with_params("https://api.giphy.com/v1/gifs/search", &[
-        ("api_key", api_key),
-        ("q", query),
-        ("limit", "50"),
-        ("rating", "g")
-    ]).unwrap();
-
-
-    let body: GiphyResponse = reqwest::get(url)?
-        .json()?;
-
-    return Ok(body.data);
-}
+mod giphy;
 
 fn main() {
     let matches = App::new("Giphy")
         .version("0.1.0")
         .author("Michael Enger <michaelenger@live.com>")
         .about("Searches giphy.com for an appropriate gif")
-        .arg(Arg::with_name("api_key")
-            .short("k")
-            .long("apikey")
-            .value_name("key")
-            .help("API key for communicating with Giphy")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("query")
-            .help("Text to use when searching for a gif")
-            .required(true)
-            .index(1))
+        .arg(
+            Arg::with_name("api_key")
+                .short("k")
+                .long("apikey")
+                .value_name("key")
+                .help("API key for communicating with Giphy")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("query")
+                .help("Text to use when searching for a gif")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
     let query = matches.value_of("query").unwrap();
     let api_key = matches.value_of("api_key").unwrap();
 
-    let results = match make_request(&api_key, &query) {
+    let results = match giphy::search(&api_key, &query) {
         Err(e) => panic!("Failed to retrieve gifs: {}", e),
         Ok(giphys) => (giphys),
     };
